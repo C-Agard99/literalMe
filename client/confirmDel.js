1ed770c7966a7d568fca5a057c9d5782bdd6bace
ChanelAgard <chanel.agard99@gmail.com>
2021-05-20 23:47:09 -0400

    took out test code

--- client/confirmDel.js
+++ client/confirmDel.js
@@ -1,7 +1,7 @@
 Template.confirmDel.events({
     'click .js-confirmDel'(event){
         let delID = $('#confirmID').val();
-        console.log("deleting documents", delID);
+        
       
         // $('#deleteModal').modal('hide');
         // //Delete permanently
